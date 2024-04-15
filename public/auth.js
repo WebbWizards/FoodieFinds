f5c2d7ae5d886ee3114a8d2eda82518ed3ebee38
KaylaBaksh <157691631+KaylaBaksh@users.noreply.github.com>
2024-04-15 21:10:48 +0000

    Anonymous auth

--- public/auth.js
+++ public/auth.js
@@ -1,70 +1,44 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-      import { 
-        getAuth, 
-        createUserWithEmailAndPassword,
-        signInWithEmailAndPassword,
-        signOut
-      } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
-      import firebaseConfig from "./firebaseConfig.js";
-
-      const app = initializeApp(firebaseConfig);
-
-      const auth = getAuth();
-
-      signUpBtn.addEventListener('click', (e) =>{
-
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed up 
-                const user = userCredential.user;
-                // Add redirect here???
-                alert('User created!');
-                window.location.assign("/home.html");
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            
-            alert(errorMessage);
-        });
+import { 
+  getAuth, 
+  signOut, 
+  signInAnonymously, 
+  setPersistence, 
+  browserLocalPersistence, 
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import firebaseConfig from "./firebaseConfig.js";
 
 
-      });
+const app = initializeApp(firebaseConfig);
 
-      signInBtn.addEventListener('click', (e) =>{
+const auth = getAuth();
 
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
+function setAuthListeners(onLogin, onLogout){
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      onLogin();
+    } else {
+      onLogout();
+    }
+  });
+}
 
-        signInWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            // ...
-            alert('Successful sign in!');
-            window.location.assign("/home.html");
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(errorMessage);
-          });
+async function signIn(){
+  try{
+    await setPersistence(auth, browserLocalPersistence);
+    const user = await signInAnonymously(auth);
+  }catch(e){
+    console.error(e);
+  }
+}
 
-      });
+async function logout() {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error('Error signing out', error);
+  }
+}
 
-      logOutBtn.addEventListener('click', (e) =>{
-
-        signOut(auth).then(() => {
-          // Sign-out successful.
-          alert('Successful sign out!');
-          window.location.assign("/");
-        }).catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          alert(errorMessage);
-        });
-
-      });
+export {auth, setAuthListeners, signIn, logout};
--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAkvoIRbn8Kpz3yfhxY40tCZSkt40UfZvw",
-    authDomain: "foodiefindsapp-31d5c.firebaseapp.com",
-    projectId: "foodiefindsapp-31d5c",
-    storageBucket: "foodiefindsapp-31d5c.appspot.com",
-    messagingSenderId: "441043092974",
-    appId: "1:441043092974:web:350e8b02cf6ddb3264baff"
-  };
+  apiKey: "AIzaSyAkvoIRbn8Kpz3yfhxY40tCZSkt40UfZvw",
+  authDomain: "foodiefindsapp-31d5c.firebaseapp.com",
+  projectId: "foodiefindsapp-31d5c",
+  storageBucket: "foodiefindsapp-31d5c.appspot.com",
+  messagingSenderId: "441043092974",
+  appId: "1:441043092974:web:350e8b02cf6ddb3264baff"
+};
 
-  export default firebaseConfig
+export default firebaseConfig;
